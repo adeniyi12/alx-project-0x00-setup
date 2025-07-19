03c98312c83ef9d891e31d4d0b9817110bb9f2c6
Timilehin Adeleye <timi.adeleye@babbangona.com>
2025-07-19 22:47:56 +0100

    fix: button props

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,7 +1,7 @@
 import { ButtonProps } from "@/interfaces";
 
-const Button: React.FC<ButtonProps> = ({ title, style }) => {
-  return <button className={style}>{title}</button>;
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return <button className={styles}>{title}</button>;
 };
 
 export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,9 +6,9 @@ const Landing: React.FC =  () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button title="Click Me" style="bg-blue px-4 py-4 border-1 mx-2 rounded-sm"/>
-      <Button title="Click Me" style="bg-green px-6 py-6 border-2 mx-4 rounded-md"/>
-      <Button title="Click Me" style="bg-yellow px-8 py-8 border-3 rounded-lg"/>
+      <Button title="Click Me" styles="bg-blue px-4 py-4 border-1 mx-2 rounded-sm"/>
+      <Button title="Click Me" styles="bg-green px-6 py-6 border-2 mx-4 rounded-md"/>
+      <Button title="Click Me" styles="bg-yellow px-8 py-8 border-3 rounded-full"/>
     </div>
   )
 }
