ddac2fc940f05e2f59a795224b88749dd5a2481b
Timilehin Adeleye <timi.adeleye@babbangona.com>
2025-07-19 22:49:56 +0100

    fix: button component

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -8,6 +8,7 @@ const Landing: React.FC =  () => {
       <Card />
       <Button title="Click Me" styles="bg-blue px-4 py-4 border-1 mx-2 rounded-sm"/>
       <Button title="Click Me" styles="bg-green px-6 py-6 border-2 mx-4 rounded-md"/>
+      <Button title="Click Me" styles="bg-yellow px-8 py-8 border-3 rounded-lg"/>
       <Button title="Click Me" styles="bg-yellow px-8 py-8 border-3 rounded-full"/>
     </div>
   )
